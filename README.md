5ef0f10747a5887b297d66b0a4d8a3df8a2936c9
craftyBLUE <62440666+craftyBLUE@users.noreply.github.com>
2025-10-21 12:54:34 +0200

    Updated to 1.1
    NOT BACKWARDS COMPATIBLE WITH 1.0

--- README.md
+++ README.md
@@ -3,11 +3,24 @@
 
 A is a nonnegative integer in base 10, it represents a location in memory.  
 The result from the binary operation `[0] NAND a` is stored at location `0`. The **old value** from location 0 is moved to position A. 
-`a` here is the value at position `A`; `[0]` is value at position `0`  
-Input is repeated until the program halts. (A1 A2 A3 A4 A5 A6 ...)  
+`a` here is the value at position `A`; `[0]` is value at position `0`    
 4294967296 (=2^32) bits of memory are available.  
 Memory does not guarantee any initial state.  
+
+# Memory I/O
 Setting value 1 to location `2` in memory halts the program and also dumps the memory to stdout.  
+
+## Output
+Setting value 1 to location `3` in memory prints to the screen the ascii character located between and including location `16` and `23`. 
+
+## Input
+Setting value 1 to location `4` in memory inputs an ascii character from the keyboard and puts its binary values between and including positions `24` and `31` in memory.  
+
+# Running code
+Put your Nand1 code in a file with a `.nand1` extension and run it like so:  
+> nand1.exe hello_world.nand1  
+
+> ./nand1 hello_world.nand1  
 
 See also  
 [craftyBLUE/Nand32](https://github.com/craftyBLUE/Nand32)  
--- nand1.cpp
+++ nand1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 char memory[1 << 29]; // 2^32 bits
 bool get(int pos) {
-  return memory[pos / 8] & (1 << (pos % 8));
+  return memory[pos / 8] & (1 << (7 - pos % 8));
 }
 void set(int pos, bool value) {
-  memory[pos / 8] = memory[pos / 8] & ~(1 << (pos % 8)) | ((int)value << (pos % 8));
+  memory[pos / 8] = memory[pos / 8] & ~(1 << (7 - pos % 8)) | ((int)value << (7 - pos % 8));
 }
 
 bool nand(bool x, bool y) {
@@ -15,18 +17,57 @@ bool nand(bool x, bool y) {
 
 int a, b;
 
-int main() {
+vector<int> commands = {};
+int ci = 0;
+
+/* MemoryIO map
+0 -> register
+1 -> helper register
+2 -> signal halt
+3 -> signal outputWrite
+4 -> signal inputRead
+5..15 -> reserved
+16..23 -> output char
+24..32 -> input char
+*/
+void memoryIO() {
+  if (get(3)) { // outputWrite (bit)
+    if (memory[2]) { // null edge case
+      printf("%c", memory[2]); // bits [16, 23]
+    }
+    set(3, 0);
+  }
+  if (get(4)) { // inputRead (bit)
+    char tempChar;
+    scanf("%c", &tempChar); // bits [24, 31]
+    memory[3] = tempChar;
+    set(4, 0);
+  }
+  return;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("No source code specified!\n");
+    return -1;
+  }
+  
+  ifstream sourceCode(argv[1]);
+  while (sourceCode >> a) {
+    commands.push_back(a);
+  }
+  sourceCode.close();
+  
   while (not get(2)) { //halt when value at position 2 is = 1
-    cin >> a;
+    a = commands[ci];
     b = nand(get(0), get(a));
     set(a, get(0));
     set(0, b);
+    
+    memoryIO();
+    
+    ci++;
+    ci %= commands.size();
   }
-  for (int i = 1; i < (1 << 29); i++) {
-    if (memory[i]) {
-      printf("%c", memory[i]);
-    }
-  }
-  printf("\n");
   return 0;
 }
